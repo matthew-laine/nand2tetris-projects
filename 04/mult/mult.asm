487a7d95f53a71607e7f0cb0d136bc07fc59f068
matthew-laine <matthew.i.laine@gmail.com>
2019-08-29 09:24:05 -0400

    mult.asm code works

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,40 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// SETUP
+// Set R2 value to zero
+@R2
+M=0
+
+// R0 contains the number we add over and over again to R2
+// R1 is our counter
+
+// Check to see if R0 or R1 is less than or equal to zero. If so, then end.
+@R0
+D=M
+@END
+D;JLE
+@R1
+D=M
+@END
+D;JLE
+
+(LOOP)
+// Add R0 to R2
+@R0
+D=M
+@2
+M=D+M
+
+// Decrement R1
+@R1
+M=M-1
+
+// GOTO LOOP if R1>0
+D=M
+@LOOP
+D;JGT
+
+(END)
+@END
+0;JMP
